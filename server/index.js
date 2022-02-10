2a1cd3b8f740d13d9d44dd310f3cd8ad6576f9ea
Parth Gala <parthpgala@gmail.com>
2022-02-10 10:49:17 +0530

    Finishing touches for heroku deployment

--- server/index.js
+++ server/index.js
@@ -14,13 +14,10 @@ app.use(cors())
 app.use('/posts', postRoutes)
 
 app.get('/', (req, res) => {
-    
     res.send('Hello to PictureBook')
 })
-// const CONNECTION_URL = "mongodb+srv://ParthGala:ParthGala2343@cluster0.ltjvc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`${PORT}`))
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    // .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
